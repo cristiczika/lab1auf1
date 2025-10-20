45f956a9b7a37a61385fd4302bb7340a08cbf79e
cristiczika <cristianczika6@gmail.com>
2025-10-20 17:27:07 +0300

    1)

--- src/main/task/GradeUtils.java
+++ src/main/task/GradeUtils.java
@@ -1,4 +1,24 @@
 package main.task;
 
 public class GradeUtils {
+
+    public static int[] getFailingGrades(int[] grades) {
+        int count = 0;
+        for (int i = 0; i < grades.length; i++) {
+            if (grades[i] < 40) {
+                count++;
+            }
+        }
+
+        int[] result = new int[count];
+        int index = 0;
+        for (int i = 0; i < grades.length; i++) {
+            if (grades[i] < 40) {
+                result[index++] = grades[i];
+            }
+        }
+
+        return result;
+    }
+
 }
